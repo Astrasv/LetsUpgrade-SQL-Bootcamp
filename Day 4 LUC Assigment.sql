f0e5d7c06a1f2def19e9ca05b4fa727b15e92875
Sudharsan Vanamali <116169969+Astrasv@users.noreply.github.com>
2023-04-06 19:42:27 +0530

    Day 4

---
+++ Day 4 LUC Assigment.sql
@@ -0,0 +1,6 @@
+/*Retrieve all unique values of the "category" column from the "products" table.
+Suppose you have a "products" table with the following data*/
+
+
+SELECT DISTINCT(category)
+FROM products;
